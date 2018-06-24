66726535bb8d72f65eb5043256e64960b7261dcd
twdeveloper <twDeveloper@hotmail.com>
2018-06-24 16:41:49 +0800

    v1

---
+++ WebApplication1/WebApplication1/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class student
+    {
+        public string name { get; set; }
+        public string tel { get; set; }
+        public string address { get; set; }
+    }
+}
